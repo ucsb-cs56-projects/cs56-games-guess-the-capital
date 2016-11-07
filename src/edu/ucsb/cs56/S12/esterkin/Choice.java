aa2eeeeab0d84ca7afd10598a313eccec757b209
Armin Mahini <amahini@umail.ucsb.edu>
2016-11-07 00:41:43 -0800

    Started Menu class and MainMenu, need to wrap up its other subclasses and Choice

---
+++ src/edu/ucsb/cs56/S12/esterkin/Choice.java
@@ -0,0 +1,22 @@
+/**
+ * Model that keeps track of the current session.
+ *
+ * @author Sean Shelton, Armin Mahini
+ * @version UCSB CS56, F16
+ */
+import java.util.ArrayList;
+
+public class Choice {
+    
+    private String choice;
+    
+    private ArrayList<String> choices;
+
+    Choice(){
+	choices = new ArrayList<String>();
+    }
+    
+    
+ 
+    
+}
---
+++ src/edu/ucsb/cs56/S12/esterkin/GameView.java
@@ -0,0 +1,27 @@
+/**
+ * View  class that handles what menu the player sees during teh game.
+ *
+ * @author Sean Shelton, Armin Mahini
+ * @version UCSB CS56, F16
+ */
+import java.util.ArrayList;
+import java.io.*;
+
+public class GameView{
+    
+    /**
+     * Used to store various menus the player will see throughout the game
+     */
+    private ArrayList<Menu> menuList = new ArrayList<Menu>();
+    
+
+    /**
+     * Decides what I display given the user's input
+     */
+    public void display(){
+	
+    }
+   
+}
+    
+
