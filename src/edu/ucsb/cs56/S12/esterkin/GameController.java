1d578278e506730968dfac322bd3e9694b5dc108
Armin Mahini <amahini@umail.ucsb.edu>
2016-11-09 21:53:00 -0800

    Need to finish Question and FinalMenu portions of GameView and GameController

--- src/edu/ucsb/cs56/S12/esterkin/GameController.java
+++ src/edu/ucsb/cs56/S12/esterkin/GameController.java
@@ -5,6 +5,8 @@
  * @version UCSB CS56, F16
  */
 import java.util.ArrayList;
+import java.io.*;
+import java.util.Scanner;
 
 public class GameController{
     
@@ -29,17 +31,43 @@ public class GameController{
     public void runGame(){
 		input = view.menuIO(GameView.MAIN_MENU); //change to static vars in GameView later, didn't compile with static vars for some reason
 		// TODO: set GameData to their selection
-		
+		if(input == 0)
+		    GameData us = new GameData(capitalsFile, statesFile);
+		else if(input == 1)
+		    GameData world = new GameData(worldCapitalsFile, worldCountriesFile);
+		else if(input == 2)
+		    GameData all = new GameData();
+	   
 		input = view.menuIO(GameView.QUESTION_NUM_MENU);
 		// TODO: set SessionModel's number of questions
+		SessionModel ten = new SessionModel();
+		if(input == 1)
+		    SessionModel.setNumQuestions(50);
+		else if(input == 2){
+		    System.out.println("Enter the number of questions");
+		    Scanner numIn = new Scanner(System.in);
+		    int customNum = numIn.nextInt();
+		    SessionModel.setNumQuestions(customNum);
+		}
 
 		// for (int i = 1; i <= model.getNumQuestions(); ++i) {	
         	input = view.menuIO(GameView.QUESTION_MENU);
 		// TODO: check if answer is right or wrong, then increase or decrease num correct
 		// might have to have gameview as an observer to sessionmodel to make this work
+		for (int i = 1; i <= model.getNumQuestions(); ++i){
+		    
+		}
 
-		input = view.menuIO(GameView.FINAL_MENU);
+		input = view.menuIO(GameView.FINAL_MENU); //need to double check with Sean/Jack
 		// if input is 1, play again
-    }    
+		try{
+		    Scanner numIn = new Scanner(System.in);
+		    int playAgain = numIn.nextInt();               
+		} catch (IOException ioe) {
+		    System.out.println("An unexpected error occured.");    
+		}
+		if(playagain != 1)
+		    break;
+    }
 
 }
--- src/edu/ucsb/cs56/S12/esterkin/GameView.java
+++ src/edu/ucsb/cs56/S12/esterkin/GameView.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.io.*;
 import java.util.Scanner;
 
-public class GameView{
+public class GameView{ //extends Observable?
     
     /**
      * Used to store various menus the player will see throughout the game
@@ -21,22 +21,40 @@ public class GameView{
     public static int FINAL_MENU = 3;
     //private Controller controller;
 
+    //initialized these in order to pass through capitals/territories for game that user wants to play (?)
+    GameData usGame = new GameData(capitalsFile, statesFile);
+    GameData worldGame = new GameData(worldCapitalsFile, worldCountriesFile);
+    GameData allGame = new GameData();
+
     GameView(){
 		menuList.add(new MainMenu());
-		menuList.add(new QuestionMenu());
 		menuList.add(new QuestionNumMenu());
+		menuList.add(new QuestionMenu());
 		menuList.add(new FinalMenu());
     }
 
     /**
      * Decides what I display given the user's input
      */
-    public int menuIO(int menuNum){
+    public int menuIO(int menuNum){//separated into 3 blocks for when the user is on the first 2 Menus (block 1), when they're on QuestionMenu (block 2), and for when they're on FinalMenu
+	if(menuNum == 0 || menuNum == 1){
 		menuList.get(menuNum).printMenu();
 		Scanner input = new Scanner(System.in);
 		int i = input.nextInt();
 		return i;
 		//controller.menuInput(i); //stub for Controller
+	}
+	else if(menuNum == 2){
+	    menuList.get(menuNum).printMenu();//TODO: need a way to pass through ArrayList<Territory> possibleChoices, Territory answerTerritory, int questionNum
+	                                      //(aka know how many questions and what kinds of questions the user wants here)
+	    Scanner input = new Scanner(System.in);
+	    int i = input.nextInt();
+	    return i;
+	}
+	else if(menuNum == 3){//TODO: need a way to pass through int numCorrect, int numQuestions
+	                     //(aka know how many questions and what kinds of questions the user wants here)
+
+	}
     }
    
 }
