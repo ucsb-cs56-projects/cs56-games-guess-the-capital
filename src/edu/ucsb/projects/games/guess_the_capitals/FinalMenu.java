fc53507180296338caeb37da78c261bd49e29d86
Armin Mahini <Armin@169-231-135-114.wireless.ucsb.edu>
2016-11-20 23:54:02 -0800

    Almost finished sorting highscores

--- src/edu/ucsb/projects/games/guess_the_capitals/FinalMenu.java
+++ src/edu/ucsb/projects/games/guess_the_capitals/FinalMenu.java
@@ -13,8 +13,9 @@ import java.util.ArrayList;
 public class FinalMenu extends Menu{
 
     File myFile = new File("HighScores.txt");
-    ArrayList <String> lines = new ArrayList<String>();
-    ArrayList <String[]> parsed = new ArrayList<String[]>();
+    HashMap <String, Integer> lines = new HashMapt <String,Integer>();
+    //HashMap <String, Integer> result = new HashMap <String, Integer>();
+    //ArrayList<String[]> parsed = new ArrayList<String[]>();
 
     /** Prints last menu the user sees once the game is over. Displays the users score and gives
      *  them the option to play again or end the session
@@ -26,12 +27,16 @@ public class FinalMenu extends Menu{
 		System.out.println("\n\n\n");
 		writeScore(menuData);
 		getScores();
-		parseLines(lines);
-		//sort(parsed);
-		printScores(parsed);
+		//sortByValue(lines);
+		//parseLines(lines);
+		//sort(parsed):
+		printScores(lines);
 		System.out.println("Enter 1 to play again (entering anything else quits the game)");
     }
-
+    /**
+     * Writes scores and names from text file to a HashMap
+     * @param menuData is passed through to access the player's score
+     */
     public void writeScore(MenuData menuData){
 	System.out.println("Please enter your username: ");
 	Scanner scanner = new Scanner(System.in);
@@ -48,8 +53,11 @@ public class FinalMenu extends Menu{
 	    ex.printStackTrace();
 	}    
     }
-
-    public ArrayList<String> getScores(){
+    /** 
+     * Reads high scores file and parses names from scores to save them in a HashMap
+     * @return a HashMap with the players' names and their corresponding scores
+     */
+    public HashMap<String, Integer> getScores(){
 	int count = 0;
 	try {
 	    FileReader fileReader = new FileReader(myFile);
@@ -57,9 +65,12 @@ public class FinalMenu extends Menu{
 	    //String line = null;
 	    //while ((line = bufferedReader.readLine()) != null){
 	    Scanner scanner = new Scanner(fileReader);
-	    while (scanner.hasNextLine() && count < 15){
+	    while (scanner.hasNextLine() && count < 20){
 		//lines.add(bufferedReader.readLine());
-		lines.add(scanner.nextLine());
+		//lines.add(scanner.nextLine());
+		String line = scanner.nextLine();
+		String[] result = line.split("\t");
+		lines.put(result[0], Integer.parseInt(result[1]));
 		count++;
 		//System.out.println(scanner.nextLine());
 		//fileScanner.nextLine();		 
@@ -72,49 +83,50 @@ public class FinalMenu extends Menu{
 	return lines;
     }
 
-    public ArrayList<String[]> parseLines(ArrayList<String> lines){
+    /*public ArrayList<String[]> parseLines(ArrayList<String> lines){
 	for (String l: lines){
 	    parsed.add(l.split("\t"));
 	}
 	return parsed;
-    }
+	}*/
+    /**
+     * Generic sort function for HashMap to organize player's names in order of the score they got
+     * @param map collection of generic types K and V (names and scores for the purpose of my game)
+     * @return result is the sorted map
+     */
+    /*public static <K, V extends Comparable<? super V>> Map<K, V>
+                                sortByValue( Map<K, V> map)
+    {
+        List<Map.Entry<K, V>> list =
+            new LinkedList<Map.Entry<K, V>>(map.entrySet());
+        Collections.sort( list, new Comparator<Map.Entry<K, V>>()
+                          {
+                              public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
+                              {
+                                  return (o1.getValue()).compareTo( o2.getValue() );
+                              }
+                          } );
 
-    
-
-    /*public void sort(ArrayList<String[]> parsed){
-	boolean swapped = true;
-	int j = 0;
-	String[] temp = new String[2];
-	String first;
-	String second;
-	for (int i = 0; i < parsed.size() - j; i++){
-            while(swapped){
-		swapped = false;
-		j++;
-		first = (parsed.get(i))[1];
-		second = (parsed.get(i+1))[1];
-		if (Integer.parseInt(first) > Integer.parseInt(second)){
-		    //temp = parsed.get(i).clone();
-		    temp[0] = (parsed.get(i))[0]; //set contents of temp to those of the larger array at i
-		    temp[1] = (parsed.get(i))[1];
-		    //parsed.get(i) = parsed.get(i+1).clone(); //set array with the larger score to the array with the lower score
-		    parsed.get(i)[0] = (parsed.get(i+1))[0]; //set contents of larger array at i to those of smaller array at i+1
-		    parsed.get(i)[1] = (parsed.get(i+1))[1];
-		    //parsed.get(i+1) = temp.clone(); //set array with lower score to array with
-		    parsed.get(i+1)[0] = temp[0];
-		    parsed.get(i+1)[1] = temp[1];
-		    swapped = true;
-		}
-	    } 
-	}
+        Map<K, V> result = new LinkedHashMap<K, V>();
+        for (Map.Entry<K, V> entry : list)
+            {
+                result.put( entry.getKey(), entry.getValue() );
+            }
+        return result;
     }*/
-    
-    public void printScores(ArrayList<String[]> parsed){
-	for (String[] p: parsed){
-	    for (int i = 0; i < p.length; i++){
-		System.out.println(p[i]);
-	    }
+
+    /**
+     * Prints players' names and their corresponding scores
+     * @param lines is the HashMap
+     */
+    public void printScores(HashMap<String, Integer> lines){
+	Iterator it = result.entrySet().iterator();
+	while (it.hasNext()){
+	    HashMap.Entry pair = (HashMap.Entry)it.next();
+	    System.out.println(pair.getKey() + "=" + pair.getValue());
+	    it.remove();
 	}
     }
+
     
 }
